f80934f78af35b235a2d90c9176104ca8be17d30
fauge7 <biwydakid50@gmail.com>
2015-12-12 00:30:53 -0700

    Sucess!!!

---
+++ desktop/src/com/fauge/games/twobuttontakeover/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.fauge.games.twobuttontakeover.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.fauge.games.twobuttontakeover.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
